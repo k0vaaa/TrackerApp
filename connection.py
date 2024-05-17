992d774eeeee5fdef629020bd743d4e215abaf8f
k0vaaa <kovalikalex03@mail.ru>
2024-05-18 01:09:26 +0300

    Added DB connection
    Created new connection file with logic of querys

---
+++ connection.py
@@ -0,0 +1,75 @@
+from PySide6 import QtWidgets, QtSql
+
+class Data:
+    def __init__(self):
+        super(Data,self).__init__()
+        self.createConnection()
+
+    def createConnection(self):
+        db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+        db.databaseName('data.db')
+
+        if not db.open():
+            QtWidgets.QMessageBox.critical(None,"Не удается установить соединение с базой данных.",QtWidgets.QMessageBox.Cancel)
+            return False
+
+        query = QtSql.QSqlQuery()
+        query.exec("CREATE TABLE IF NOT EXISTS data (ID integer primary key AUTOINCREMENT, Date VARCHAR(20),"
+                   "Category VARCHAR(20), Description VARCHAR(20), Balance REAL, Status VARCHAR(20))")
+        return True
+
+    def executeQueryWithParams(self, sql_query, query_values = None):
+        query = QtSql.QSqlQuery()
+        query.prepare(sql_query)
+
+        if query_values is not None:
+            for query_value in query_values:
+                query.addBindValue(query_value)
+
+        query.exec()
+        return query
+    def addQuery(self, date, category, description, balance, status):
+        sql_query = "INSERT INTO data (Date, Category, Description, Balance, Status) VALUES (?,?,?,?,?)"
+        self.executeQueryWithParams(sql_query,[date,category,description, balance,status])
+
+    def updateQuery(self, date, category, description, balance, status, id):
+        sql_query = "UPDATE data SET Date=?, Category=?, Description=?, Balance=?, Status=? WHERE ID=?"
+        self.executeQueryWithParams(sql_query,[date,category,description, balance,status,id])
+
+    def deleteQuery(self,id):
+        sql_query = "DELETE * FROM data  WHERE ID=?"
+        self.executeQueryWithParams(sql_query,[id])
+
+    def getTotal(self, column, filter = None, value = None):
+        sql_query= f"SELECT SUM({column}) FROM data"
+        if filter is not None and value is not None:
+            sql_query += f"WHERE {filter} = ?"
+
+        query_values = []
+
+        if value is not None:
+            query_values.append()
+
+        query = self.executeQueryWithParams(sql_query, query_values)
+
+        if query.next():
+            return str(query.value(0)) + "руб."
+        return '0'
+
+    def totalBalance(self):
+        return  self.getTotal(column="Balance")
+
+    def totalIncome(self):
+        return self.getTotal(column="Balance",filter="Status", value="Поступление")
+
+    def totalOutcome(self):
+        return self.getTotal(column="Balance",filter="Status", value="Списание")
+
+    def totalFood(self):
+        return self.getTotal(column="Balance",filter="Category", value="Еда")
+
+    def totalEnter(self):
+        return self.getTotal(column="Balance",filter="Category", value="Развлечения")
+
+    def totalSubs(self):
+        return self.getTotal(column="Balance",filter="Category", value="Подписки")
